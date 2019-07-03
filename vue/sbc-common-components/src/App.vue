5fddb5c3a6671535a386c51ec181cc40d82470a1
Jeremy Dunsdon <twenty15creative@gmail.com>
2019-07-03 11:21:53 -0700

    Fixing Issues w/ Vuetify & Layout Updates

--- vue/sbc-common-components/src/App.vue
+++ vue/sbc-common-components/src/App.vue
@@ -1,25 +1,21 @@
 <template>
-  <v-app id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <sbc-layout>
     <router-view/>
-  </v-app>
+  </sbc-layout>
 </template>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+<script>
+import Vue from 'vue'
+import SbcLayout from '@/components/SbcLayout.vue'
+export default Vue.extend({
+  name: 'app',
+  components: {
+    SbcLayout
+  }
+})
+</script>
+
+<style lang="stylus">
+  @import "./assets/styl/base.styl";
+  @import "./assets/styl/overrides.styl";
 </style>
