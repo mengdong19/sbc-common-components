00051852c686e2c08f181954b463ae5aabc336c1
Jeremy Dunsdon <twenty15creative@gmail.com>
2019-07-03 11:21:53 -0700

    Updating Sign-in Button

--- vue/sbc-common-components/src/components/SbcHeader.vue
+++ vue/sbc-common-components/src/components/SbcHeader.vue
@@ -7,9 +7,9 @@
                      title="Province of British Columbia"/>
             </a>
             <div class="app-header__actions">
-            <span v-if="authorized"><button class="v-btn" @click="logout">Sign Out</button> </span>
-            <!-- temporary have "sign in" button which also trigger logout for unit test before figure out session mock for test case. Can be removed later.-->
-            <span v-else><button class="v-btn" @click="logout">Sign In</button> </span>
+              <span v-if="authorized"><button class="v-btn v-btn--outline v-btn--depressed theme--light" @click="logout">Sign Out</button></span>
+              <!-- temporary have "sign in" button which also trigger logout for unit test before figure out session mock for test case. Can be removed later.-->
+              <span v-else><button v-btn v-btn--outline v-btn--depressed theme--light @click="logout">Sign In</button></span>
             </div>
         </div>
     </header>
@@ -72,6 +72,8 @@ export default Vue.extend({
 
       .v-btn
         margin-right 0
+        color #ffffff
+        border-color #ffffff
 
     .brand-img
         margin-top 0.3rem
